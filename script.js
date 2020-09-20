afed13a46e561e1d2d9df766136345b0b9b108de
Darien <darienweise@gmail.com>
2020-09-20 19:01:58 -0400

    refresh prevent and more results for variety added

--- script.js
+++ script.js
@@ -14,73 +14,89 @@ movieQuizApp.getResults = () => {
         data: {
             // From TMDB documentation
             api_key: movieQuizApp.key,
-            language: "en-US",
+            language: "en",
             sort_by: "popularity.desc",
             // Figure out how to get just one page
             page: 1,
-        }
+            page: 2,
+            page: 3,
+         }
     }).then((res) => {
+       
+      // Display 2 random movies generated from a random item function
 
-        console.log(res.results);
-        // movieQuizApp.getResults(res);
+      // Display random movie for left image from random item function
+      const firstMovie = movieQuizApp.randomMovie(res.results)
+      $('.movie1 img').attr({
+         'src': `https://image.tmdb.org/t/p/original${firstMovie.poster_path}`,
+         'alt': firstMovie.title
+      });
 
-        // Display 2 random movies generated from a random item function
-        const firstMovie = movieQuizApp.randomMovie(res.results)
-        $('.movie1 img').attr({
-            'src': `https://image.tmdb.org/t/p/original${firstMovie.poster_path}`,
-            'alt': firstMovie.title
-        });
-        $('.firstMovieTitle').text(firstMovie.title);
+      // add movie title to left image on screen
+      $('.firstMovieTitle').text(firstMovie.title);
 
-        
-        console.log(firstMovie);
+      // Display random movie for right image from random item function
+      const secondMovie = movieQuizApp.randomMovie(res.results)
+      $('.movie2 img').attr({
+         'src': `https://image.tmdb.org/t/p/original${secondMovie.poster_path}`,
+         'alt': secondMovie.title
+      });
 
-        const secondMovie = movieQuizApp.randomMovie(res.results)
-        $('.movie2 img').attr({
-            'src': `https://image.tmdb.org/t/p/original${secondMovie.poster_path}`,
-            'alt': secondMovie.title
-        });
-        $('.secondMovieTitle').text(secondMovie.title);
-
-        console.log(secondMovie);
-        
-        // user clicks on button
-        $('button').on('click', function(){
-            const selectedMovie = $(this);
-            const buttonSelected = $(this);
-            // console.log(buttonSelected.attr(‘id’))
-
-
-
-            // console.log(selectedMovie.attr('id'));
-            // console.log(movieQuizApp.selectedMovie.attr('id'));
-
-            if (selectedMovie === 'movie1' && firstMovie.popularity > secondMovie.popularity) {
-                console.log('yes!');
-            } else {
-                console.log('no');
+      // add movie title to right image on screen
+      $('.secondMovieTitle').text(secondMovie.title);
+      
+      ////////// NEXT TWO functions for user results of clicked movie ////////
+      //
+      // when user clicks on #movie1
+      $('#movie1').on('click', function(){
+      // show if the user choice is correct or incorrect
+         if (firstMovie.popularity > secondMovie.popularity) {
+            // unhide #clickResult>#correctAnswer to display answer on page
+            console.log('correct');
+            $('#answer').text('Correct!');
+         }else {if (firstMovie.popularity < secondMovie.popularity)
+            $('#answer').text('Incorrect!');
+         }
+      })
+      // when user clicks on #movie1
+      $('#movie2').on('click', function(){
+         // show if the user choice is correct or incorrect
+            if (firstMovie.popularity < secondMovie.popularity) {
+               $('#answer').text('Correct!');
+            }else {if (firstMovie.popularity > secondMovie.popularity)
+               $('#answer').text('Incorrect!');
             }
-        })
-        // compare the movie the user selected to the movie determined to be more popular
-        // if user guessed right … do something
-        // else … do something else
-
-
-        // const toDisplay = () => {
-        // $('.movie1').click(function(){
-        //     $('.button').on('click', function(){
-        //     $(this).html()
-        //     })
-        //     })
-        // }
-
+         }) 
+         
+      // Prevents two movies from populating the same
+         if (firstMovie.popularity === secondMovie.popularity){
+               location.reload();
+         }
     })
 }
 
+// randomizes results of the array and outputs two objects
 movieQuizApp.randomMovie = (array) => {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 }
+
+// init function that will kick off the app:
+movieQuizApp.init = () => {
+    movieQuizApp.getResults();
+}
+
+// document ready:
+$(document).ready(function () {
+    movieQuizApp.init();
+})
+
+
+
+/////////////////////////// Psuedo code
+// 
+// if user guessed right … do something
+// else … do something else
 // Create an if statement that prevents two movies with same title
 // let i = (res.title)
 // if (i === i) then run random movie again until result is different
@@ -97,12 +113,19 @@ movieQuizApp.randomMovie = (array) => {
 // Add points system & display final score
 // Display alert with final score
 // Find a way to make the app to continue indefinitely
+       
 
-// Create init function that will kick off the app:
-movieQuizApp.init = () => {
-    movieQuizApp.getResults();
-}
-// Create document ready:
-$(document).ready(function () {
-    movieQuizApp.init();
-})
+/////////////////////// MISC CODE SNIPPETS
+// 
+// const toDisplay = () => {
+        // $('.movie1').click(function(){
+        //     $('.button').on('click', function(){
+        //     $(this).html()
+        //     })
+        //     })
+        // }
+
+
+                    // console.log(buttonSelected.attr(‘id’))
+            // console.log(selectedMovie.attr('id'));
+            // console.log(movieQuizApp.selectedMovie.attr('id'));
