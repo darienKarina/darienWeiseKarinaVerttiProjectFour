0e6d665cfae69c80c6a09cfb38bb093b29fb6698
Karina <kary.vertti@gmail.com>
2020-09-20 22:31:49 -0400

    results styles and alerts

--- script.js
+++ script.js
@@ -20,10 +20,7 @@ movieQuizApp.getResults = () => {
             page: 1,
         }
     }).then((res) => {
-
-        console.log(res.results);
-        // movieQuizApp.getResults(res);
-
+        
         // Display 2 random movies generated from a random item function
         const firstMovie = movieQuizApp.randomMovie(res.results)
         $('.movie1 img').attr({
@@ -31,10 +28,7 @@ movieQuizApp.getResults = () => {
             'alt': firstMovie.title
         });
         $('.firstMovieTitle').text(firstMovie.title);
-
         
-        console.log(firstMovie);
-
         const secondMovie = movieQuizApp.randomMovie(res.results)
         $('.movie2 img').attr({
             'src': `https://image.tmdb.org/t/p/original${secondMovie.poster_path}`,
@@ -42,67 +36,65 @@ movieQuizApp.getResults = () => {
         });
         $('.secondMovieTitle').text(secondMovie.title);
 
-        console.log(secondMovie);
         
-        // user clicks on button
-        $('button').on('click', function(){
-            const selectedMovie = $(this);
-            const buttonSelected = $(this);
-            // console.log(buttonSelected.attr(‘id’))
+      ////////// NEXT TWO functions for user results of clicked movie ////////
+      // 
 
-
-
-            // console.log(selectedMovie.attr('id'));
-            // console.log(movieQuizApp.selectedMovie.attr('id'));
-
-            if (selectedMovie === 'movie1' && firstMovie.popularity > secondMovie.popularity) {
-                console.log('yes!');
-            } else {
-                console.log('no');
+      // when user clicks on #movie1
+        $('#movie1').on('click', function(){
+        // if movie displayed inside #movie1 is *MORE popular than in #movie2 
+            if (firstMovie.popularity > secondMovie.popularity) {
+            $('#answer').text('Correct!');
+            $('#movie1').css("border", "10px solid green");
+            }else {if (firstMovie.popularity < secondMovie.popularity)
+            $('#answer').text('Incorrect!');
+            $('#movie1').css("border", "10px solid red");
             }
         })
-        // compare the movie the user selected to the movie determined to be more popular
-        // if user guessed right … do something
-        // else … do something else
+        // when user clicks on #movie2
+        $('#movie2').on('click', function(){
+           // if movie displayed inside #movie1 is *LESS popular than in #movie2 
+           // check if user has already answered
+           // if use did not answer move on
+           // else do nothing
+            if (firstMovie.popularity < secondMovie.popularity) {
+                $('#answer').text('Correct!');
+                $('#movie2').css("border", "10px solid green");
+            }else {if (firstMovie.popularity > secondMovie.popularity)
+                $('#answer').text('Incorrect!');
+                $('#movie2').css("border", "10px solid red");
+            }
+        })  
+        })
 
+        $( ".refresh" ).click(function() {
+            // set userAnswered to false
+            location.reload();
+        });
 
-        // const toDisplay = () => {
-        // $('.movie1').click(function(){
-        //     $('.button').on('click', function(){
-        //     $(this).html()
-        //     })
-        //     })
-        // }
+    }
 
-    })
-}
-
-movieQuizApp.randomMovie = (array) => {
+    movieQuizApp.randomMovie = (array) => {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
-}
-// Create an if statement that prevents two movies with same title
-// let i = (res.title)
-// if (i === i) then run random movie again until result is different
-// let userScore = 0;
-// Create an event listener for a 'click' event in the movie buttons
-// Save information from user choice into a variable
-// Determine if user choice is correct or not
-// Display a message saying if user is right or wrong
-// Display NEXT button to go to next pair of movies
-// Hide NEXT button until user selects an option again
-// Repeat from here 3 more times 
-// Or start over
-// Stretch goals:
-// Add points system & display final score
-// Display alert with final score
-// Find a way to make the app to continue indefinitely
+    }
 
-// Create init function that will kick off the app:
-movieQuizApp.init = () => {
+    // track if the user has selected an answer
+    movieQuizApp.userAnswered = false;
+
+  // Create init function that will kick off the app:
+    movieQuizApp.init = () => {
     movieQuizApp.getResults();
-}
-// Create document ready:
-$(document).ready(function () {
+    }
+  // Create document ready:
+    $(document).ready(function () {
     movieQuizApp.init();
-})
+    })
+    
+    
+  /////////////////////////// Pseudo code
+
+  // Stretch goals:
+  // Add points system & display final score
+  // Display alert with final score
+  // Find a way to make the app to continue indefinitely
