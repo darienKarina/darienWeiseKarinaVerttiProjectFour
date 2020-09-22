94b8f67c1d654a18f25d76c8e948fc73a2c2d054
Darien <darienweise@gmail.com>
2020-09-21 21:31:44 -0400

    extra CSS add on, and code clean up

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Create a namespace
 const movieQuizApp = {};
 
-// The URL I use to make my API call:
+// URL from API 
 movieQuizApp.endpoint = "https://api.themoviedb.org/3/discover/movie";
 
-// The API kei I use to access the APi:
+// movieDB API key
 movieQuizApp.key = "1c6216962b83757c0807c889023d20c0";
+
+// Pulling data from movieDB API
 movieQuizApp.getResults = () => {
     $.ajax({
         url: movieQuizApp.endpoint,
@@ -17,18 +18,18 @@ movieQuizApp.getResults = () => {
             language: "en-US",
             sort_by: "popularity.desc",
             // Figure out how to get just one page
-            page: 1,
-            page: 2,
-            page: 3
+            page: 4,
         }
     }).then((res) => {
-        
-        // Display 2 random movies generated from a random item function
+
+      // Adds movie poster images to movies displayed on page
         const firstMovie = movieQuizApp.randomMovie(res.results)
         $('.movie1 img').attr({
             'src': `https://image.tmdb.org/t/p/original${firstMovie.poster_path}`,
             'alt': firstMovie.title
         });
+
+         // Displaying movie title
         $('.firstMovieTitle').text(firstMovie.title);
         
         const secondMovie = movieQuizApp.randomMovie(res.results)
@@ -36,62 +37,63 @@ movieQuizApp.getResults = () => {
             'src': `https://image.tmdb.org/t/p/original${secondMovie.poster_path}`,
             'alt': secondMovie.title
         });
+
         $('.secondMovieTitle').text(secondMovie.title);
 
         
-      ////////// NEXT TWO functions for user results of clicked movie ////////
+      ////////// NEXT TWO functions display results of user click  ////////
       // 
-
       // when user clicks on #movie1
         $('#movie1').on('click', function(){
         // if movie displayed inside #movie1 is *MORE popular than in #movie2 
             if (firstMovie.popularity > secondMovie.popularity) {
-            $('#answer').text('Correct!');
-            $('#movie1').css("border", "10px solid green");
+               $('#answer').text('Correct!');
+               $('#movie1').css("border", "10px solid green");
             }else {if (firstMovie.popularity < secondMovie.popularity)
-            $('#answer').text('Incorrect!');
-            $('#movie1').css("border", "10px solid red");
+               $('#answer').text('Incorrect!');
+               $('#movie1').css("border", "10px solid red");
             }
-        })
+         })
         // when user clicks on #movie2
         $('#movie2').on('click', function(){
            // if movie displayed inside #movie1 is *LESS popular than in #movie2 
-           // check if user has already answered
-           // if use did not answer move on
-           // else do nothing
             if (firstMovie.popularity < secondMovie.popularity) {
-                $('#answer').text('Correct!');
-                $('#movie2').css("border", "10px solid green");
+               $('#answer').text('Correct!');
+               $('#movie2').css("border", "10px solid green");
             }else {if (firstMovie.popularity > secondMovie.popularity)
-                $('#answer').text('Incorrect!');
-                $('#movie2').css("border", "10px solid red");
+               $('#answer').text('Incorrect!');
+               $('#movie2').css("border", "10px solid red");
             }
-        })  
         })
+      //  Prevents two movies of same value from loading at same time
+        if (firstMovie === secondMovie){
+           location.reload();
+        }
+      })
 
-      //  prevent two same movies from being populated at the same time.
-        $( ".refresh" ).click(function() {
-            location.reload();
-        });
+   //loads new pair of movies when clicked
+   $( ".refresh" ).click(function() {
+      location.reload();
+      });
+   }
 
-    }
+   // Randomizes result of movieQuizApp movies displayed
+   movieQuizApp.randomMovie = (array) => {
+      const randomIndex = Math.floor(Math.random() * array.length);
+      return array[randomIndex];
+   }
 
-    movieQuizApp.randomMovie = (array) => {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
-    }
+   // tracks if the user has selected an answer
+   movieQuizApp.userAnswered = false;
 
-    // track if the user has selected an answer
-    movieQuizApp.userAnswered = false;
-
-  // Create init function that will kick off the app:
-    movieQuizApp.init = () => {
-    movieQuizApp.getResults();
-    }
-  // Create document ready:
-    $(document).ready(function () {
-    movieQuizApp.init();
-    })
+  // Init function that starts movieQuizApp
+   movieQuizApp.init = () => {
+      movieQuizApp.getResults();
+   }
+  // When document is ready, initialize movieQuizApp
+   $(document).ready(function () {
+      movieQuizApp.init();
+   })
     
     
   /////////////////////////// Pseudo code
